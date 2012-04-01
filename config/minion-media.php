d505c44fdc1d43c39dddb03088968414e78e8b0d
Evan Purkhiser <evanpurkhiser@gmail.com>
2012-04-01 06:04:16 -0400

    Much better configuration and a clear definition of how things will work

--- config/minion-media.php
+++ config/minion-media.php
@@ -1,60 +1,65 @@
 <?php
 
 return array(
-	/**
-	 * Use symlinks when possible (used when compiling files)
-	 * Disable this if your filesystem has issues with the `ln` command
-	 */
-	'symlinks' => TRUE,
+
+	// Where should the media source files reside
+	'sources' => APPPATH.'media-src',
+
+	// Setup the different compilers to run, each compiler will recursively
+	// run through the search and locate files that match the pattern. After
+	// finding all files that match the pattern it will then pass the file
+	// list to the compiler, which takes a set of options to compile the sources
 	'compilers' => array(
+
+		// SASS CSS pre-processor,
+		'sass' => array(
+
+			// The class used to compile SASS
+			'class' => 'Media_Compiler_SASS',
+
+			// The path to search for SASS files
+			'search_path' => 'stylesheets',
+
+			// Regex that the file name must match to be compiled
+			'pattern' => '/^[^_].*\.sass$/',
+
+			// Options for the sass compiler
+			'options' => array(
+
+				// Valid style options are compressed, expaneded, and nested
+				'style' => 'nested',
+
+				// Make compass includes available?
+				'compass' => FALSE,
+
+				// Where to cache the pre-compiled partials
+				'cache_path' => APPPATH.'cache',
+
+				// Where to save compiled sass files to
+				'output' => APPPATH.'media',
+			),
+		),
+
+		// Javascript compressor using uglify-js
 		'js' => array(
-			// Regular expression the path must match
-			'pattern' => '/^(media\/js\/).*\.js$/',
-			'class'     => 'Media_Compiler_JS',
-			'options'   => array(
-				// Options for concatenating JS files
-				'concat' => array(
-					// Group name
-					'main' => array(
-						// Files get sorted by this before being merged
-						'order' => 1,
-						// Files belong to this group if the filepath matches the pattern
-						'pattern' => '/^(media\/)js.*/',
-					),
-				),
-				// Where to save the concatenated JS
-				'save_paths' => array(
-					// Version best used in production (FALSE will disable)
-					'minified'   => APPPATH.'media/js/compiled/app.min.js',
-					// Unminified version for development
-					'unminified' => APPPATH.'media/js/compiled/app.js',
-				),
-			),
-		),
-		'scss' => array(
-			// Regular expression the path must match
-			'pattern' => '/^(media\/css\/scss\/).*\.scss$/',
-			'class'     => 'Media_Compiler_SCSS',
-			'options'   => array(
-				// Compass project settings (relative from tmp_dir)
-				'css_dir'         => 'media/css/compiled',
-				'sass_dir'        => 'media/css/scss',
-				'output_style'    => ':compressed',
-				'images_dir'      => 'media/images/',
-				'relative_assets' => TRUE,
-				// Where we create the temporary compass project
-				'tmp_dir'         => APPPATH.'cache/compass',
-				// We append css_dir to this path before saving compiled css files
-				'save_dir'        => rtrim(APPPATH, '/'),
-			),
-		),
-		'couchapp' => array(
-			// Regular expression the path must match
-			'pattern' => '/^(media\/couchapp\/).*/',
-			'class'     => 'Media_Compiler_CouchApp',
-			'options'   => array(
-				// Where we create the temporary couchapp directory
-				'tmp_dir'         => APPPATH.'cache/couchapp',
+
+			// The class to use to compile the javascript files
+			'class' => 'Media_Compiler_JS',
+
+			// The path to search for Javascript files
+			'search_path' => 'javascript',
+
+			// Regex that the filename must mach to be compiled
+			'pattern' => '/^.*\.js$/',
+
+			// Options for the javascript compiler
+			'options' => array(
+
+				// Beautify the javascript code (indent it mostly)
+				'beautify' => Kohana::$environment !== Kohana::PRODUCTION,
+
+				// Where to save the compiled concatinated file
+				'output' => APPPATH.'media/application.js',
 			),
 		),
 	),
